3b731bf814864b01d720a87f2f4e2f031ef0ea49
owlism <galileogalilei787@gmail.com>
2024-10-12 22:23:40 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,6 @@
 class CalendarsController < ApplicationController
 
+
   # １週間のカレンダーと予定が表示されるページ
   def index
     getWeek
@@ -12,10 +13,13 @@ class CalendarsController < ApplicationController
     redirect_to action: :index
   end
 
+  def new
+  end
+
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
